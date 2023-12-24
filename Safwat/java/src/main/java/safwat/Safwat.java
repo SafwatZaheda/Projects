69f77befefe4f77178f65a670b21289288356ee5
SafwatZaheda <154759978+SafwatZaheda@users.noreply.github.com>
2023-12-24 21:59:21 +0200

    Add files via upload

---
+++ Safwat/java/src/main/java/safwat/Safwat.java
@@ -0,0 +1,27 @@
+
+
+package safwat;
+
+
+
+public class Safwat {
+
+    public static void main(String[] args) {
+        int[] nums = {15, 7, 11 ,2};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0] + ", " + result[1]);
+    }
+    
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target)
+                    return new int[]{i, j};
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    }
+    
+
